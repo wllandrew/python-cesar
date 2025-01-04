0f6dfcd9bfb2f00edb261fb5fe70a5192f584909
wllandrew <andrewwillian331@gmail.com>
2025-01-04 20:43:09 -0300

    More performance

--- main.py
+++ main.py
@@ -12,102 +12,51 @@ class Cifra:
         with open("config.json", 'r') as Config:
             Configurations = json.load(Config)
 
-        self.Mode = Configurations['mode']
+        self.Mode = Configurations['Mode']
+        self.Key = Configurations['Key']
 
         if self.Mode == "bruteforce":
             self.BruteForce()
         else:
             self.Encrypt()
 
-        self.Key = Configurations['key']
+    # Método que encriptografa/descriptografa o texto
+    def Encrypt(self):
+        
+        # Reduz os modos a uma só chave
+        key = self.Key if self.Key == "encrypt" else (26 - self.Key) % 26
+
+        with open(self.fileName, 'w') as File:
+            File.write(Cifra.CaesarAlgorithm(key, self.Text))
 
     
-    def Encrypt(self):
-        pass
-    
+    # Método força bruta para achar uma key descriptografadora
     def BruteForce(self) -> int | list[int]:
         pass
 
+    # Método estático que implementa o algoritmo de criptografagem de césar
     @staticmethod
     def CaesarAlgorithm(key : int, text : str) -> str:
-        pass
+        encrypt = ''
 
-def Main():
-    cif = Cifra("texto.txt")
-
-if __name__ == "__main__":
-    Main()
-
-
-"""
-class Cifra:
-    def __init__(self, name: str):
-        self.name = name
-
-        with open(self.name, 'r') as doc:
-            self.texto = doc.read()
-
-        with open('config.json', 'r') as js:
-            config = json.load(js)
-
-        self.modo = config['modo']
-        self.chave = config['chave']
-
-    def ler(self):
-        print(self.texto)
-
-    def escrever(self):
-        if self.modo == 'forcabruta':
-            self.forcaBruta()
-        else: 
-            with open(self.name, 'w') as txt:
-                txt.write(self.criptografar(self.texto, self.chave))
-
-    @staticmethod
-    def criptografar(texto, chave : int) -> str:
-        stringDecript = []
-        for n in texto:
-            if ord(n) > 64 and ord(n) < 91:
-                stringDecript.append(chr((ord(n) - 65 + chave) % 26 + 65))
-            elif ord(n) > 96 and ord(n) < 123:
-                stringDecript.append(chr((ord(n) - 97 + chave) % 26 + 97))
+        for character in text:
+            
+            # Problema: não inclui caracteres especiais, como acentos
+            if character.isalpha():
+                if ord(character) >= 65 and ord(character) <= 90:
+                    encrypt += chr((ord(character) - 65 + key) % 26 + 65)
+                else:
+                    encrypt += chr((ord(character) - 97 + key) % 26 + 97)
             else:
-                stringDecript.append(n)
-        return ''.join(stringDecript)
+                encrypt += character
 
-    @staticmethod
-    def maisFrequente(lst : list) -> int | None:
-        valor = 0
-
-        for i in lst:
-            atualmaior = lst.count(i)
-            if atualmaior > contador:
-                contador = atualmaior
-                valor = i
-        
-        return valor
-
-    def forcaBruta(self):
-    
-        with open('palavrasComuns.txt', 'r', encoding="UTF-8") as pc:
-                self.palavrasComuns = pc.read()
-                self.palavrasComuns = self.palavrasComuns.split("\n")
-
-        resp = []
-        for word in self.palavrasComuns:
-            for n in range(26):
-                if word in self.criptografar(self.texto, n) and len(word) > 2:
-                    resp.append(n)
-
-        ch = self.maisFrequente(resp)
-
-        print(f'Chave: {ch}\n Texto: {self.criptografar(self.texto, ch)}')
+        return encrypt
+            
 
 def Main():
     cif = Cifra("texto.txt")
-    cif.escrever()
 
 if __name__ == "__main__":
     Main()
 
-"""
+
